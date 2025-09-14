6086f2a58fb2dc6c8b875f9c23858a6014272537
khangttse182098 <khangthaitran246@gmail.com>
2025-09-14 16:19:09 +0700

    feat: add milvus

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from pandas.io.html import pprint_thing
+from pymilvus import MilvusClient
 from fastapi import FastAPI, Form, Response, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from insightface.app import FaceAnalysis
 import cv2
 import numpy as np
+
+client = MilvusClient("./milvus.db")
+if not client.has_collection("face_embedding"):
+    client.create_collection(collection_name="face_embedding", dimension=512)
 
 app = FastAPI()
 
@@ -27,6 +33,7 @@ async def verify_person(res: Response, comparedImg: UploadFile = Form(...)):
     multi_embeddings = []
 
     for img_path in multi_images:
+        # prepare for model
         img = cv2.imread(img_path)
         faces = model.get(img)
         if len(faces) > 0:
@@ -39,13 +46,25 @@ async def verify_person(res: Response, comparedImg: UploadFile = Form(...)):
     # Convert bytes to numpy array
     nparr = np.frombuffer(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    faces_new = model.get(img)
+    compared_face = model.get(img)
 
-    if len(faces_new) > 0:
-        new_embedding = faces_new[0].normed_embedding
+    if len(compared_face) > 0:
+        compared_face_embedding = compared_face[0].normed_embedding
         
+        # Add temp data
+        # tmp_db_data = [{"id": k, "code": 10, "vector": v} for k, v in enumerate(multi_embeddings)]
+        # db_res = client.insert(collection_name="face_embedding", data=tmp_db_data)
+
+        # Get current user
+        db_res = client.query(
+            collection_name="face_embedding",
+            filter="code == 10",
+            output_fields=["vector"],
+        )
+        # print(db_res)
+
         # Compute similarity (cosine distance) to all target embeddings
-        similarities = [np.dot(new_embedding, e) for e in multi_embeddings]
+        similarities = [np.dot(compared_face_embedding, e) for e in multi_embeddings]
         
         # Take the maximum similarity
         max_similarity = max(similarities)
